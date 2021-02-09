8868d88ee78d3b1ecd8c1ae7ca3446ca7e89e896
Phubohdee Areesawangkit <phubohdee_bill@hotmail.com>
2021-02-09 11:55:20 +0700

    Refactor to use array.forEace

--- math.js
+++ math.js
@@ -1,9 +1,10 @@
 function sum(a, b){
     if(Array.isArray(a)) {
         let total = 0
-        for (let i = 0 ; i < a.length ; i++){
-            total += a[i]
-        }
+        // for (let i = 0 ; i < a.length ; i++){
+        //     total += a[i]
+        // }
+        a.forEach((n) => (total += n))
         return total
     }
     return a + b;
